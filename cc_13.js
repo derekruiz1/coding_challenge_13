b391256552b624348d601f7d48c329203287b20f
Derek Ruize <derekruiz@usf.edu>
2025-03-09 13:20:52 -0400

    Task 5 - Inline Editing for Employee Cards

--- cc_13.js
+++ cc_13.js
@@ -18,16 +18,55 @@ document.addEventListener("DOMContentLoaded", () => {
         removeButton.textContent = "Remove";
         removeButton.addEventListener("click", (event) => {
             card.remove();
-            event.stopPropagation
+            event.stopPropagation();
         });
         card.appendChild(removeButton);
 
-        employeeContainer.appendChild(card);
-    }
+ //Task 5 - Incline Editing of Employee Details
 
-    addEmployeeCard("Lebron James", "Player");
-    addEmployeeCard("Walter White", "Chemist");
-});
+ const editButton = document.createElement("button"); //Making button to edit card
+ editButton.textContent = "Edit";
+ editButton.addEventListener("click", () => {
+     enableEditing(card, nameHeading, positionParagraph, editButton, removeButton);
+ });
+
+ card.appendChild(nameHeading);
+ card.appendChild(positionParagraph);
+ card.appendChild(editButton);
+ card.appendChild(removeButton);
+ employeeContainer.appendChild(card);
+}
+
+function enableEditing(card, nameHeading, positionParagraph, editButton, removeButton) { //Editing the card
+ const nameInput = document.createElement("input");
+ nameInput.value = nameHeading.textContent;
+
+ const positionInput = document.createElement("input");
+ positionInput.value = positionParagraph.textContent;
+
+ const saveButton = document.createElement("button"); //Making button to save changes
+ saveButton.textContent = "Save";
+
+ saveButton.addEventListener("click", () => { 
+     nameHeading.textContent = nameInput.value;
+     positionParagraph.textContent = positionInput.value;
+
+     card.innerHTML = "";
+     card.appendChild(nameHeading);
+     card.appendChild(positionParagraph);
+     card.appendChild(editButton);
+     card.appendChild(removeButton);
+ });
+
+ card.innerHTML = "";
+ card.appendChild(nameInput);
+ card.appendChild(positionInput);
+ card.appendChild(saveButton);
+}
+
+addEmployeeCard("Lebron James" , "Player")
+addEmployeeCard("Walter White" , "Chemist")
+})
 
 //Task 3 - Converting NodeLists to Arrays for Bulk Updates
 document.addEventListener("DOMContentLoaded", function () {
